5838be4c054eaa9fa3f53d1374e2593286148afc
Ross Crichton <rcrichton87@googlemail.com>
2017-05-08 15:39:04 +0100

    day 1 homework finished, index and create routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'api/shows' => 'shows#index'
+
+  post 'api/shows' => 'shows#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
